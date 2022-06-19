8fa2f90f6e58f4bc9c55e2f84beb66bb805caf2e
Jimi-Bada <jimibadaadedamola@gmail.com>
2022-06-19 20:10:30 +0100

    commit

--- js/app.js
+++ js/app.js
@@ -86,7 +86,7 @@ function createPost(e){
                 `
         }});   
          
-        postwrapper.innerHTML = postHolder;
+        postContainer.innerHTML = postHolder;
     
          
             
